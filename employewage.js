d8db996f766769b064f67e463f8ba1a398a1e63f
Rajas1312 <you@example.com>
2021-01-07 15:35:20 +0530

    Added :code to check attandance

--- employewage.js
+++ employewage.js
@@ -1,1 +1,10 @@
 console.log("welcome to employee wage computation");
+function checkAttandance() {
+    let random=Math.round(Math.random());
+    if(random=1){
+        console.log("Employe is present")
+    }else{
+        console.log("Employee is absent")
+    }   
+}
+checkAttandance();
