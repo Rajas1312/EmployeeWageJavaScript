61577ede3d9557ad720310652e47b5882ea8ad85
Rajas1312 <you@example.com>
2021-01-07 16:55:45 +0530

    Added:code to calculate monthly wages

--- employewage.js
+++ employewage.js
@@ -8,24 +8,35 @@ function checkAttandance() {
     }   
 }
 checkAttandance();
+let wagePerHr=20;
+let fullDay=8;
+let halfDay=4
+let daily=0;
+const fullTime=1
+const partTime=0
 function calculateDailyWage() {
-    let wagePerHr=20;
-    let fullDay=8;
-    let halfDay=4
-    const fullTime=1
-    const partTime=0
+   
     let random=Math.round(Math.random());
     switch(random){
         case fullTime:
             let dailywage=wagePerHr*fullDay;
+            daily=dailywage;
             console.log(dailywage);
             break
         case partTime:
             let dailywage1=wagePerHr*halfDay;
             console.log(dailywage1);
+            daily=dailywage1;
             break;
         default :
         console.log("invalid") 
     }    
 }
 calculateDailyWage();
+
+function calculateMonthlyWage(){
+    let workingDaysPerMonth=20;
+    let monthlyWage=workingDaysPerMonth*daily;
+    console.log(" monthly wageis "+monthlyWage)
+}
+calculateMonthlyWage();
